7c8760c79c918e57578cfd7381e56c9fe54c5142
belalhanafy <belalhanafy373@gmail.com>
2025-07-09 13:07:33 +0300

    updates

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -10,12 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "react-chat-app-9f0f1.firebaseapp.com",
-  projectId: "react-chat-app-9f0f1",
-  storageBucket: "react-chat-app-9f0f1.firebasestorage.app",
-  messagingSenderId: "585859428080",
-  appId: "1:585859428080:web:d0c5ea0a7373e4a7389c35"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
